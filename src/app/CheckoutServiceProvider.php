4246700c164e54ae89e8a8b671a5ef03552a9664
Matheus Ferreira <matheusfs97@gmail.com>
2020-05-07 15:59:57 -0300

    fix: loading routes

--- src/app/CheckoutServiceProvider.php
+++ src/app/CheckoutServiceProvider.php
@@ -9,11 +9,11 @@ class CheckoutServiceProvider extends ServiceProvider {
     public function register() {
 
         $this->app->make('MatheusFS\LaravelCheckoutPagarMe\CheckoutFacade');
-        $this->loadViewsFrom(dirname(__DIR__).'/views', 'checkout');
     }
     
     public function boot() {
         
-        include __DIR__.'/routes.php';
+        $this->loadViewsFrom(dirname(__DIR__).'/views', 'checkout');
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
 }
