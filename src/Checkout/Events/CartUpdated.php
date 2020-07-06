0bca85ba63c1cf7981d6c9c00bfe63825d22790a
Matheus Ferreira <matheusfs97@gmail.com>
2020-07-06 04:08:42 -0300

    f

--- src/Checkout/Events/CartUpdated.php
+++ src/Checkout/Events/CartUpdated.php
@@ -2,8 +2,8 @@
 
 namespace MatheusFS\Laravel\Checkout\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -23,6 +23,6 @@ class CartUpdated implements ShouldBroadcast {
 
     public function broadcastOn() {
 
-        return new PrivateChannel("user:$this->user_id");
+        return new Channel("user:$this->user_id");
     }
 }
--- src/Checkout/routes.php
+++ src/Checkout/routes.php
@@ -4,10 +4,10 @@ Route::namespace('MatheusFS\Laravel\Checkout')->group(function(){
 
     Route::post('checkout/pagarme/postback/orders', 'Payment\Gateways\PagarMe\Postback@orders')->name('checkout.pagarme.postback.orders');
     Route::post('checkout/pagarme/postback/transactions', 'Payment\Gateways\PagarMe\Postback@transactions')->name('checkout.pagarme.postback.transactions');
-    Route::post('checkout/{cart_key}', 'Controllers\CartController@checkout')->name('checkout');
+    Route::post('checkout', 'Controllers\CartController@checkout')->name('checkout');
 
-    Route::post('cart/count', 'Controllers\CartController@count')->name('cart.count');
-    Route::post('cart/html', 'Controllers\CartController@html')->name('cart.html');
-    Route::post('cart/add', 'Controllers\CartController@add')->name('cart.add');
-    Route::post('cart/remove', 'Controllers\CartController@remove')->name('cart.remove');
+    // Route::post('cart/count', 'Controllers\CartController@count')->name('cart.count');
+    // Route::post('cart/html', 'Controllers\CartController@html')->name('cart.html');
+    // Route::post('cart/add', 'Controllers\CartController@add')->name('cart.add');
+    // Route::post('cart/remove', 'Controllers\CartController@remove')->name('cart.remove');
 });
