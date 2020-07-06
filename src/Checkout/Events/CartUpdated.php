18d06f68763a3d6ec6458046dc16233a768043ab
Matheus Ferreira <matheusfs97@gmail.com>
2020-07-06 05:45:27 -0300

    f

--- src/Checkout/Events/CartUpdated.php
+++ src/Checkout/Events/CartUpdated.php
@@ -4,6 +4,7 @@ namespace MatheusFS\Laravel\Checkout\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -23,6 +24,6 @@ class CartUpdated implements ShouldBroadcast {
 
     public function broadcastOn() {
 
-        return new Channel("user:$this->user_id");
+        return new PrivateChannel("user:$this->user_id");
     }
 }
