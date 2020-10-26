6a4e7ef388a4efe4a10fe9bb667f78ad39d39d5f
Matheus Ferreira <matheusfs97@gmail.com>
2020-10-26 13:55:50 -0300

    Update routes.php

--- src/Checkout/routes.php
+++ src/Checkout/routes.php
@@ -1,9 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
+
 Route::namespace('MatheusFS\Laravel\Checkout')->group(function(){
 
     Route::post('checkout/pagarme/postback/orders', 'Payment\Gateways\PagarMe\Postback@orders')->name('checkout.pagarme.postback.orders');
     Route::post('checkout/pagarme/postback/transactions', 'Payment\Gateways\PagarMe\Postback@transactions')->name('checkout.pagarme.postback.transactions');
+    
+    Route::post('checkout/pagarme/capture', function(Request $request){
+        $capturedTransaction = $pagarme->transactions()->capture([
+            'id' => $request->token,
+            'amount' => $request->amount
+        ]);
+    })->name('checkout.pagarme.capture');
 
     // Route::post('cart/count', 'Controllers\CartController@count')->name('cart.count');
     // Route::post('cart/html', 'Controllers\CartController@html')->name('cart.html');
