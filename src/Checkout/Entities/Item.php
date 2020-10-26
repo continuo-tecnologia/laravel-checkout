707d324d19ea8477fd152e9e2dfb97203ca28015
Matheus Ferreira <matheusfs97@gmail.com>
2020-10-26 14:02:25 -0300

    Update Item.php

--- src/Checkout/Entities/Item.php
+++ src/Checkout/Entities/Item.php
@@ -8,13 +8,15 @@ class Item {
     public $title;
     public $unit_price;
     public $quantity = 1;
+    public $tangible = true;
 
-    public function __construct($id, $title, $unit_price, $quantity = 1) {
+    public function __construct($id, $title, $unit_price, $quantity = 1, $tangible = true) {
         
         $this->id = $id;
         $this->title = $title;
         $this->unit_price = $unit_price;
         $this->quantity = $quantity;
+        $this->tangible = $tangible;
     }
 
     public function price(){
