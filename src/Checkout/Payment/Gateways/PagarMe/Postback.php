aad53234a6f35c9a41f80e8541129a5bbb387dd6
Matheus Ferreira <matheusfs97@gmail.com>
2020-10-26 14:26:56 -0300

    Update Postback.php

--- src/Checkout/Payment/Gateways/PagarMe/Postback.php
+++ src/Checkout/Payment/Gateways/PagarMe/Postback.php
@@ -26,29 +26,11 @@ class Postback {
             __FUNCTION__
         );
         
-        if($normalized['status'] == 'paid'){
-
-            return response('<script type="text/javascript">
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,"script",
-                "https://connect.facebook.net/en_US/fbevents.js");
-
-                fbq("init", "562881037919202"); 
-                fbq("track", "purchase");
-            </script>');
-        }else{
-        
-            return response()->json([
-                'error' => null,
-                'message' => 'Postback order received correctly!',
-                'order_id' => $request->id
-            ]);
-        }
+        return response()->json([
+            'error' => null,
+            'message' => 'Postback order received correctly!',
+            'order_id' => $request->id
+        ]);
     }
 
     public function transactions(Request $request) {
