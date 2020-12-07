380a96cb91fb14b4d829a592565b45b609d5f10a
Matheus Ferreira <matheusfs97@gmail.com>
2020-12-07 09:47:08 -0300

    refact: config stub

--- config/checkout.php
+++ config/checkout.php
@@ -1,24 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Storage;
-
 return [
 
-    'name' => 'REFRESHER Shop',
+    'name' => env('APP_NAME') . ' Shop',
 
     'disk' => 's3',
 
-    'logo' => Storage::disk('s3')->url('loja/logo1.png'),
+    'logo' => '',
 
     'date_timezone' => 'America/Sao_Paulo',
 
     'mailling' => [
 
-        'from' => env('MAIL_FROM_ADDRESS', 'contato@refreshertrends.com.br'),
+        'from' => env('MAIL_FROM_ADDRESS', 'example@domain.com'),
 
         'copies' => [
-            'matheus@refresher.com.br',
-            'marketplace@refresher.com.br'
+            'example2@domain.com'
         ]
     ],
 
@@ -27,19 +24,19 @@ return [
         'model' => App\Models\User\Supplier::class,
         'property_mapping' => [
             'logo' => 'logo',
-            'name' => 'nome_empresa',
+            'name' => 'name',
             'email' => 'email'
         ]
     ],
 
     'pagarme' => [
 
-        'api_key' => 'ak_live_uLF749Vstvw6jeNx8AH5uroEH0XAC9',
-        'api_sandbox_key' => 'ak_test_ZfkuJKLEYICsa9IB38dmMDDCc9nvHH',
+        'api_key' => 'ak_live_xyz',
+        'api_sandbox_key' => 'ak_test_xyz',
     ],
 
     'facebook' => [
 
-        'pixel_id' => '562881037919202'
+        'pixel_id' => '000'
     ]
 ];
